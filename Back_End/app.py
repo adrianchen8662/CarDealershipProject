89d7b47fb0fced914ba4071f28b72f380426b9cd
Adrian Chen <adrianchen8662@gmail.com>
2024-12-08 22:49:37 -0500

    forward engineering for database test

--- Back_End/app.py
+++ Back_End/app.py
@@ -1,25 +1,25 @@
 from flask import Flask
 import mysql.connector
 
-'''
+
 mydb = mysql.connector.connect(
-  host="your_database_host",
-  user="your_username",
-  password="your_password",
-  database="your_database_name"
+  host="127.0.0.1",
+  user="root",
+  password="password",
+  database="Car_Dealership_Database"
 )
-'''
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    '''
+    
     mycursor = mydb.cursor()
-    mycursor.execute("SELECT * FROM your_table_name")
+    mycursor.execute("SELECT * FROM test")
     myresult = mycursor.fetchall()
 
     for x in myresult:
         print(x)
-    '''
+    
     return "Hello World!"
